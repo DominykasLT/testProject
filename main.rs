5d7fc98158bc63886aeb37a012b569947fcdfcb4
Dominykas <55694581+DominykasLT@users.noreply.github.com>
2025-02-03 09:01:49 +0200

    test6

--- main.rs
+++ main.rs
@@ -1,9 +1,13 @@
-// src/main.rs
-
 use simple_app::process_input;
+use std::env;
 use std::io::{self, Read};
 
 fn main() {
+    if env::args().any(|arg| arg == "--version") {
+        println!("simple_app version 1.2");
+        return;
+    }
+
     let mut input = String::new();
     if let Err(err) = io::stdin().read_to_string(&mut input) {
         eprintln!("Error reading input: {}", err);
